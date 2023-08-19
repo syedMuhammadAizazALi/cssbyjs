b9615a673f16e970feaeee91a9fed09283d93b43
Muzamil Ali <uzairassociates.ali@gmailcom>
2023-08-19 02:44:31 -0700

    done

---
+++ app.js
@@ -0,0 +1,51 @@
+//question2
+
+// function change_color() {
+//     const para = document.querySelector("#paragraph")
+//     if (para.style.backgroundColor  == "blue") {
+//         para.style.backgroundColor="white";
+//         para.style.color="black";
+        
+//     }
+//     else{
+//         para.style.backgroundColor="blue";
+//         para.style.color = "white";
+//     }
+//  }
+
+
+
+
+
+// function change_color() {
+//     const para = document.querySelector("#paragraph")
+//     if (para.style.display="none") {
+//         para.style.backgroundColor="blue";
+//         para.style.color = "white";
+        
+//     }
+//     else{
+//         para.style.display="none"
+//     }
+//  }
+
+
+// qustion 3
+
+// const hiddenText = document.getElementById("hiddenText");
+// const showTextButton = document.getElementById("showTextButton");
+
+// showTextButton.addEventListener("click", function() {
+//     hiddenText.style.display = "block";
+// });
+
+
+
+
+
+// question1 
+   
+        // const paragraph = document.getElementById("myParagraph");
+        // paragraph.textContent = "Hello, DOM!";
+    
+
